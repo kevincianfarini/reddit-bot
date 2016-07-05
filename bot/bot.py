708f93c2f061f2593d06231b1dc209e16ec6380d
Kevin Cianfarini <kevincianfarini@gmail.com>
2016-07-05 11:14:52 -0400

    Scaping comments works properly

--- bot/bot.py
+++ bot/bot.py
@@ -22,11 +22,17 @@ def already_answered(comment):
 
 
 def generate_reply(requests):
-    links = []
+    reply = ''
     for request in requests:
-        cleaned_request = '' #TODO strip down to item and call get_amazon_link()
-        links.append(get_amazon_link(cleaned_request))
-    return links.join('\n\n')
+        link = (get_amazon_link(request))
+        if link is not None:
+            reply += 'Amazon: [%s](%s)' % (request, link)
+            reply += '\n\n'
+        else:
+            reply += "I couldn't find anything matching %s. Sorry!" % request
+            reply += '\n\n'
+    reply += 'I am a bot. If you have any feedback that might improve me, send a message!.'
+    return reply
 
 
 def post_reply(comment, reply):
@@ -35,10 +41,12 @@ def post_reply(comment, reply):
 
 
 def check_comments():
-    for comment in praw.helpers.comment_stream(r, 'all', limit=10):
+    for comment in praw.helpers.comment_stream(r, 'test', limit=10):
         clean_comment = remove_formatting(comment.body)
         requests = link_regex.findall(clean_comment)
         if len(requests) > 0:
             if not already_answered(comment):
                 reply = generate_reply(requests)
                 post_reply(comment, reply)
+
+check_comments() #TODO: remove this ater testing
