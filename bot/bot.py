3b74167ce8b15b6dd4f3b10eb26a0e4621ffafae
Kevin Cianfarini <kevincianfarini@gmail.com>
2016-07-09 00:02:55 -0400

    Got amazon stuff working

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,9 @@
 import re
 import praw
+from amazon.api import AmazonAPI, AmazonException
 
 r = praw.Reddit(user_agent="Kevin Cianfarini's reddit bot that sends different prices of items from different sites")
-r.login(username='kmatthewc', password='Scc102795*')
+r.login(username='AmazonItBot', password='Scc1122445102795*') #TODO do oath and put in config file
 already_answered_comments = []
 link_regex = re.compile(r'\bAmazonIt![\s]*(.*?)(?:\.|;|$)', re.M | re.I)
 
@@ -13,8 +14,13 @@ def remove_formatting(comment):
     return comment.replace("*", "").replace("~", "").replace("^", "").replace(">", "").replace("[", "").replace("]", "").replace("(", "").replace(")", "")
 
 
-def get_amazon_link(item):
-    pass
+def get_amazon_order(item):
+    amazon = AmazonAPI('AKIAJ2GNCUSCN4HAJXEA', 'hH5W7wg9gUtpPjroI8Y+7jdCIL4yr/lwEJhjEdmm', 'redditbotli03-20')
+    try:
+        products = amazon.search_n(1, Keywords=item, SearchIndex='All')
+    except AmazonException as e:
+        return None
+    return products[0]
 
 
 def already_answered(comment):
@@ -24,9 +30,10 @@ def already_answered(comment):
 def generate_reply(requests):
     reply = ''
     for request in requests:
-        link = (get_amazon_link(request))
-        if link is not None:
-            reply += 'Amazon: [%s](%s)' % (request, link)
+        product = get_amazon_order(request)
+        if product is not None:
+            product_info = product.title[:50] + '... ' + str(product.price_and_currency[0]) + ' ' + product.price_and_currency[1]
+            reply += 'Amazon: [%s](%s)' % (product_info, product.offer_url)
             reply += '\n\n'
         else:
             reply += "I couldn't find anything matching %s. Sorry!" % request
@@ -41,12 +48,12 @@ def post_reply(comment, reply):
 
 
 def check_comments():
-    for comment in praw.helpers.comment_stream(r, 'test', limit=10):
+    for comment in praw.helpers.comment_stream(r, 'test'):
         clean_comment = remove_formatting(comment.body)
         requests = link_regex.findall(clean_comment)
         if len(requests) > 0:
-            if not already_answered(comment):
+            if not already_answered(comment) and comment.author.name != 'kmatthewc':
                 reply = generate_reply(requests)
                 post_reply(comment, reply)
 
-check_comments() #TODO: remove this ater testing
+check_comments()
